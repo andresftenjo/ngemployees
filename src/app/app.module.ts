36be2c31663ef2efd2fa399a5ba5058d8fe06550
andresftenjo <useda.theli@gmail.com>
2020-02-17 23:40:42 -0500

    adding first version

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { EmployeeService } from './services/employee.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+  ],
+  providers: [EmployeeService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/employee.model.ts
@@ -0,0 +1,12 @@
+export class Employee {
+  Id:number;
+  Name: string;
+  Area:string;
+  InitDate:Date;
+  JobTitle:string;
+  Country:string;
+  UserName:string;
+  HireDate:Date;
+  Rate:string;
+  Status:boolean;
+}
